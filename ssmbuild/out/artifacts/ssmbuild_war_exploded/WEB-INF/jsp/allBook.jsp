a133e2b8ad4417ca03140b1bc386dd31d00517e2
lizhiweip <2090037702@qq.com>
2022-07-24 22:20:28 +0800

    第一次提交，大致框架构成

---
+++ ssmbuild/out/artifacts/ssmbuild_war_exploded/WEB-INF/jsp/allBook.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 李志炜
+  Date: 2022/7/24
+  Time: 15:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>书籍展示</title>
+    <%--BootStrap美化界面--%>
+    <link href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+
+</head>
+<body>
+
+<h1>书籍展示</h1>
+
+</body>
+</html>
---
+++ ssmbuild/src/main/java/com/li/service/BookServiceImpl.java
@@ -0,0 +1,43 @@
+package com.li.service;
+
+import com.li.dao.BookMapper;
+import com.li.pojo.Books;
+
+
+import java.util.List;
+//service调dao层：组合Dao
+
+public class BookServiceImpl implements BookService{
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    @Override
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    @Override
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    @Override
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    @Override
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    @Override
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+
+
+}
